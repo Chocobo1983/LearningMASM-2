f9c2a4f79be158973017b7af094cb21651040a9a
stb.infoserv <stb.infoserv@gmail.com>
2022-01-03 20:01:41 +0200

    Add project files.

---
+++ LearningMASM2/Main.asm
@@ -0,0 +1,22 @@
+
+;----------x86-64-----------------
+;.code
+;main	proc
+;		mov rax,2
+;		mov rbx,3
+;		mov rcx,4
+;		ret
+;main endp
+;end
+;----------x86-32-----------------
+.386
+.model flat
+.code
+main	proc
+		mov eax,2
+		mov ebx,3
+		mov ecx,4
+		ret
+main endp
+end main
+
