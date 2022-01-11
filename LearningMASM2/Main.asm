28dc8b5bd8e3d4b267c3cc0f458655ffd65a85cd
Chocobo1983 <stb.infoserv@gmail.com>
2022-01-11 22:27:28 +0200

    16bit added

--- LearningMASM2/Main.asm
+++ LearningMASM2/Main.asm
@@ -9,14 +9,24 @@
 ;main endp
 ;end
 ;----------x86-32-----------------
+;.386
+;.model flat
+;.code
+;main	proc
+;		mov eax,2
+;		mov ebx,3
+;		mov ecx,4
+;		ret
+;main endp
+;end main
+;----------16bit-----------------
 .386
 .model flat
 .code
 main	proc
-		mov eax,2
-		mov ebx,3
-		mov ecx,4
+		mov ax,2
+		mov bx,3
+		mov cx,4
 		ret
 main endp
 end main
-
